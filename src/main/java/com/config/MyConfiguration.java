4bf4aaeceb2209591f7c99b2a29e3cea94afce1b
Haridas250503 <haridasshinde314@gmail.com>
2025-12-30 00:51:32 +0530

    added the file

--- src/main/java/com/config/MyConfiguration.java
+++ src/main/java/com/config/MyConfiguration.java
@@ -1,37 +1,14 @@
 package com.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 import com.model.Student;
 
 @Configuration
+@ComponentScan(basePackages= {"com.model"})
 public class MyConfiguration {
 	
-	   @Bean
-       public Student m1() {
-		   Student s=new Student();
-		   s.setId(10);
-		   s.setName("haridas");
-		   s.setCity("hingoli");
-		   s.setPercentage(86.90);
-    	   System.out.println("m1 method called");
-    	   
-    	   return s;
-       }
-
-	   @Bean
-	   public void m2() {
-		   System.out.println("m2 method called");
-	   }
-	   
-	   @Bean
-	   public void m3() {
-		   System.out.println("m3 method called");
-	   }
-	   
-	   @Bean 
-	   public void m4() {
-		   System.out.println("m4 method called");
-	   }
+	  
 }
--- src/main/java/com/model/Student.java
+++ src/main/java/com/model/Student.java
@@ -1,5 +1,8 @@
 package com.model;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Student {
 	
 	private int id;
