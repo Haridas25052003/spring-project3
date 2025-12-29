ce32596fec3818c6f444f802532918d44257a4ab
Haridas250503 <haridasshinde314@gmail.com>
2025-12-30 00:54:21 +0530

    added the files

--- src/main/java/com/model/Address.java
+++ src/main/java/com/model/Address.java
@@ -6,5 +6,12 @@ public class Address {
 	private String line2;
 	private String city;
 	private String state;
+	
+	@Override
+	public String toString() {
+		return "Address [line1=" + line1 + ", line2=" + line2 + ", city=" + city + ", state=" + state + "]";
+	}
+	
+	
 
 }
