3bfe1ea58f6231ce1e4bc406df624389f4f20c94
Haridas250503 <haridasshinde314@gmail.com>
2025-12-30 00:22:07 +0530

    initial commit of eclipse projec

---
+++ src/main/java/spring_project3/spring_project3/App.java
@@ -0,0 +1,13 @@
+package spring_project3.spring_project3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
