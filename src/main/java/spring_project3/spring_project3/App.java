3a73e177873e643650391a6309f1f1e55dd501a7
Haridas250503 <haridasshinde314@gmail.com>
2025-12-30 00:46:41 +0530

    adding the file

--- src/main/java/spring_project3/spring_project3/App.java
+++ src/main/java/spring_project3/spring_project3/App.java
@@ -4,6 +4,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import com.config.MyConfiguration;
+import com.model.Student;
 
 /**
  * Hello world!
@@ -14,5 +15,8 @@ public class App
     public static void main( String[] args )
     {
         ApplicationContext context=new AnnotationConfigApplicationContext(MyConfiguration.class);
+    
+        Student s=context.getBean(Student.class);
+        System.out.println(s);
     }
 }
