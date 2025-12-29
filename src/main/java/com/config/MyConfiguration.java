b07251589b6da393fa2253654d33b08c24529d47
Haridas250503 <haridasshinde314@gmail.com>
2025-12-30 00:37:19 +0530

    add  for the testing

--- src/main/java/com/config/MyConfiguration.java
+++ src/main/java/com/config/MyConfiguration.java
@@ -4,5 +4,9 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MyConfiguration {
+	
+       public MyConfiguration () {
+    	   System.out.println("constructor for testing");
+       }
 
 }
