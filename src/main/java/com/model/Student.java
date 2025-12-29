3982a577b2f1c1f8fc86ea49ce5bacb25dfb6ad4
Haridas250503 <haridasshinde314@gmail.com>
2025-12-30 00:30:10 +0530

    added the file

--- src/main/java/com/model/Student.java
+++ src/main/java/com/model/Student.java
@@ -10,7 +10,28 @@ public class Student {
 	public String toString() {
 		return "Student [id=" + id + ", name=" + name + ", city=" + city + ", percentage=" + percentage + "]";
 	}
-	
-	
-
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public double getPercentage() {
+		return percentage;
+	}
+	public void setPercentage(double percentage) {
+		this.percentage = percentage;
+	}
 }
